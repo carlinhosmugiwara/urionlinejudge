f13226a315cd244e5737ed47e2f18de9099e5d0c
carlinhosmugiwara <55931229+carlinhosmugiwara@users.noreply.github.com>
2024-01-31 10:19:46 -0300

    Update FaculdadeFisica_1197.java

--- FaculdadeFisica_1197.java
+++ FaculdadeFisica_1197.java
@@ -9,13 +9,13 @@ import java.util.*;
 public class Main {
  
     public static void main(String[] args) throws IOException {
-    Scanner sacanner =  new Scanner(System.in);	
-do {
-int v =  sacanner.nextInt();
-int t =  sacanner.nextInt();
-System.out.println(2*v*t);
-}while(sacanner.hasNext());
- 
-    }
+      Scanner sacanner =  new Scanner(System.in);	
+      do {
+      int v =  sacanner.nextInt();
+      int t =  sacanner.nextInt();
+      System.out.println(2*v*t);
+      } while(sacanner.hasNext());
+       
+        }
  
 }
